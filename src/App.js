cf9fb3555eee04c63c6cbf791f75946eacb07d72
younessemorabih <152270266+younessemorabih@users.noreply.github.com>
2024-10-15 15:14:07 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './compenent/comp1'
+import Imag  from './compenent/comp2';
+import  Siadbar from './compenent/comp3'
+
+function App() {
+  return (
+     <>
+     
+      <Navbar>   
+      </Navbar>
+      <Siadbar/>
+      <Imag></Imag>
+      
+
+     </>
+    
+
+  );
+}
+
+export default App;
